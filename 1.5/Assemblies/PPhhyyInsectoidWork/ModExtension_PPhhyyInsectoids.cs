b0d307f8e436a58e289ddb7ce024caf8693e2bbf
Bonf6059 <87819153+Bonf6059@users.noreply.github.com>
2025-12-23 10:18:55 +0100

    Delete 1.5 directory

--- 1.5/Assemblies/PPhhyyInsectoidWork/ModExtension_PPhhyyInsectoids.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PPhhyyInsectoidWork
-{
-    /// <summary>
-    /// For the time being just a dummy mod extension to try and make the harmony prefix a little bit more secure.
-    /// </summary>
-    public class ModExtension_PPhhyyInsectoids : DefModExtension
-    {
-    }
-}
